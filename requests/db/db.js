b2484b3b6860a23173779a58400fd33e657c4564
alayor <alayor3@gmail.com>
2017-01-18 21:30:21 -0600

    Adding unit test to read from requests db

--- requests/db/db.js
+++ requests/db/db.js
@@ -2,9 +2,14 @@ var mongoskin = require('mongoskin');
 var db = mongoskin.db('mongodb://@localhost:27017/improvements', {safe:true});
 
 var insert = function(data, callback) {
-  db.collection('requests').insert(data, {}, callback);
+    db.collection('requests').insert(data, {}, callback);
+}
+
+var find = function(data, callback) {
+    db.collection('requests').findOne(data, callback);
 }
 
 module.exports = {
-    insert: insert
+    insert: insert,
+    find: find
 }
