00c8f563876d719530d1dca8575218c520c24b6b
alayor <alayor3@gmail.com>
2017-01-26 21:34:04 -0600

    Starting almost from scratch to create clean unit tests.

--- requests/api/spec.js
+++ requests/api/spec.js
@@ -1,6 +1,6 @@
 var api = require('./api')
 
-describe('improvements', function(){
+describe('improvements', function() {
   it('saves a request in bd', function(done) {
      api.func({body: {}});
      done();
