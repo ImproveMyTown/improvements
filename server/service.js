c13138ac479d99ae08e07952420d57c610e44423
alayor <alayor3@gmail.com>
2017-01-29 22:04:18 -0600

    Solving failing spec

--- server/service.js
+++ server/service.js
@@ -3,13 +3,14 @@ var express = require('express'),
   logger = require('morgan');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 
 var start = function () {
-   this.server = app.listen(process.env.PORT || 3000, function(){
-     console.log('Express server listening on port 3000')
+   this.server = app.listen(app.get('port'), function(){
+     console.log('Express server listening on port', app.get('port'))
   });
 }
 var stop = function () {
@@ -19,7 +20,7 @@ var stop = function () {
 
 
 app.get("/", function(req, res, next) {
-    return res.send("Hello World!");
+    return res.send("{success: true}");
 });
 
 var addMapping = function (api) {
